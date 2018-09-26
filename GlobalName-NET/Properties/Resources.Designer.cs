9ef4c486235029ec015cce2a61bcbfdbd8722a0c
MelissaData <github@melissadata.com>
2018-09-25 18:26:22 -0700

    Changed Namespace

--- GlobalName-NET/Properties/Resources.Designer.cs
+++ GlobalName-NET/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GlobalName_NET_REST.Properties {
+namespace GlobalName_NET.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GlobalName_NET_REST.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GlobalName_NET_REST.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GlobalName_NET.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
